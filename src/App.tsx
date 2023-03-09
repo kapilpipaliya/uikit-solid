191e111d3ecad51dfa9465e071b67a451094b0e6
Chaitanya Gupta <54992097+Chaitanya31612@users.noreply.github.com>
2023-03-09 23:30:01 +0530

    label component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import type { Component } from 'solid-js';
-
 import { newVertex } from './utils/newVertex';
 import { Vertex } from './components/Form';
 import { Label } from './components/Label/Label';
@@ -7,7 +5,7 @@ import { Label } from './components/Label/Label';
 export const App = () => {
   const meta = newVertex(0, ["Meta"], {
     id: "meta1",
-    props: { enableThreeStateBehavior: false, content: "Label", color: "primary" },
+    props: { content: "Label", color: "primary" },
   });
   const data = newVertex(0, ["Vertex"], { meta1: "Sample Label" });
 
--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -3,13 +3,15 @@ import {LabelProps} from "../../../globals";
 import {splitProps} from "solid-js";
 
 export function Label(props: ComponentProps) {
+
   const [selectedProps, customProps] = splitProps(props.meta.properties.props as LabelProps, ['color'])
 
   return (
     <div
       {...customProps}
       classList={{
-        [`uk-label-${selectedProps.color}`]: !!selectedProps.color,
+          'uk-label': true,
+          [`uk-label-${selectedProps.color}`]: !!selectedProps.color,
       }}
     >
       {props.data.properties[props.meta.properties.id]}
